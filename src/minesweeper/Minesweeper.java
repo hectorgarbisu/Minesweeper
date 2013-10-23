576869caac9684ec8f41cef6a7f621adccebcdba
63990 <63990@63990-PC>
2013-10-23 16:22:31 +0000

    Segundo Comí

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,7 +1,7 @@
 package minesweeper;
 
 public class Minesweeper {
-    //aggawg
+    //aggawgw
     public static void main(String[] args) {
         MinesweeperControl minesweeperControl = new MinesweeperControl();
         minesweeperControl.execute();
