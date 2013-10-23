13634d2f51b302419130c7a4d9d32d03be973139
63990 <63990@63990-PC>
2013-10-23 00:06:43 +0000

    Primer commit. Faltan muchas cosas.

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,7 +1,7 @@
 package minesweeper;
 
 public class Minesweeper {
-    
+    //
     public static void main(String[] args) {
         MinesweeperControl minesweeperControl = new MinesweeperControl();
         minesweeperControl.execute();
