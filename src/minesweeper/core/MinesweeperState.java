f8b2cae5712febe036740158f9b905df8d81a03c
63990 <63990@63990-PC>
2013-10-25 20:48:31 +0000

    Sexto commit; Asunto actual: El juego es funcional, pero la interfaz de texto es muy incomoda. Podria ponerle la posibilidad de plantar una banderita para marcar minas y cambiar la forma en que se visualizan las casillas vacías desocultas.

--- src/minesweeper/core/MinesweeperState.java
+++ src/minesweeper/core/MinesweeperState.java
@@ -1,23 +1,30 @@
 package minesweeper.core;
+import minesweeper.core.support.XYLocation;
+import minesweeper.core.tool.*;
 
 public class MinesweeperState {
     private boolean victory;
     private char[][] table;
-
-    public char[][] getTable() {
-        return table.clone();
+    private MinesweeperHideMask minesweeperHideMask;
+    
+    public MinesweeperState mineSweeperState(MinesweeperState state){
+        MinesweeperState newState;
+        newState = new MinesweeperState(state.getTable()[0].length,state.getTable().length);
+        return newState;        
     }
-   
-    public MinesweeperState(int width, int height) {
+    
+    public  MinesweeperState(int width, int height) {
         victory = false;
         table = new char[height][width];
         for (int i = 0; i < height; i++)
             for (int j = 0; j < width; j++) table[i][j] = ' ';
     }
    
-    public MinesweeperState(int width, int height,int numberOfMines) {
+    private MinesweeperState(int width, int height,int numberOfMines) {
         this(width,height);
-        table = MinesweeperMinesPutter.putMines(table, numberOfMines);
+        minesweeperHideMask = new MinesweeperHideMask(table);
+        table = MinesweeperMinePutter.putMines(table, numberOfMines);
+        table = MinesweeperNumberPutter.putNumbers(table);
     }
     public MinesweeperState(int[] initialValuesArray){
         this(initialValuesArray[0],initialValuesArray[1],initialValuesArray[2]);
@@ -26,12 +33,29 @@ public class MinesweeperState {
     public MinesweeperState() {
         this(8,8);
     }
+
+    public char[][] getTable() {
+        char[][] tableCopy = new char[table.length][table[0].length];
+        for (int i = 0; i < table.length; i++)
+            for (int j = 0; j < table[i].length; j++)
+                tableCopy[i][j] = table[i][j];
+        return tableCopy;
+    }
+    
+  
+    public int getWidth(){
+        return table[0].length;
+    }
+    
+    public int getHeight(){
+        return table.length;
+    }
     
     public boolean isTerminal(){
         boolean isComplete = true;
         for (int i = 0; i < table.length; i++)
             for (int j = 0; j < table[i].length; j++){
-                if(table[i][j] == ' ') isComplete = false;
+                if(table[i][j] == ' '||table[i][j]=='0') isComplete = false;
                 if(table[i][j] == 'D') {
                     victory = false;
                     return true;
@@ -40,27 +64,56 @@ public class MinesweeperState {
         victory = true;
         return isComplete;
     }
-    
     public boolean hasWon(){
         return victory;
     }
 
-    public void makeMove(int[] move) {
-        if(move[0]<table.length)
-        if(move[1]<table[0].length)
-        if(table[move[0]][move[1]]==' '){
-            mark(move);
+    public void makeMove(XYLocation move) {
+        if(move.getY()<table.length)
+        if(move.getX()<table[0].length)
+        if(table[move.getY()][move.getX()]==' '
+                ||table[move.getY()][move.getX()]=='0'
+                ||table[move.getY()][move.getX()]=='#'){
+            footPrint(move);
         }
-        else if(table[move[0]][move[1]]=='M')
+        else if(table[move.getY()][move.getX()]=='M')
             die(move);
+        else{
+            minesweeperHideMask.unHide(move);
+        }
+    }
+    
+    private void footPrint(XYLocation move) {
+        MinesweeperRecursiveUnhider minesweeperRecursiveUnhider = new MinesweeperRecursiveUnhider(this);
+        minesweeperRecursiveUnhider.doRecursiveUnhiding(move);     
+        minesweeperHideMask.unHide(minesweeperRecursiveUnhider.getUnhidenLocations());
+        /*for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                if((table[i][j]==' ')&&minesweeperHideMask.isShown(i,j)){
+                    table[i][j]='#';
+                }
+            }
+        }*/
+        //minesweeperHideMask.unHide(move);
+    }
+    
+    private void die(XYLocation move) { 
+        table[move.getY()][move.getX()] = 'D';
+        minesweeperHideMask.unHide(move);
     }
 
-    private void mark(int[] move) {
-        table[move[0]][move[1]] = 'X';
-    }
-
-    private void die(int[] move) { 
-        table[move[0]][move[1]] = 'D';
+    public char[][] getHidenTable() {
+        char[][] hidenTable = getTable(); 
+        boolean[][] hideTable = minesweeperHideMask.getHideTable();
+        for (int i = 0; i < hidenTable.length; i++)
+            for (int j = 0; j < hidenTable[i].length; j++)
+                if(hideTable[i][j])hidenTable[i][j]=' ';      
+        return hidenTable;
     }
 
 }
+
+            
+    
+
+
