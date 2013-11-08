358cc0f8544bd37ebb2df2b9228647d6edb8fff1
63990 <63990@63990-PC>
2013-11-08 22:59:20 +0000

    Ya hay clase Game. Lo siguiente deberia hacer todo pero usando ya la clase cell y prescindir de hidemask

--- src/minesweeper/TODOlist.java
+++ src/minesweeper/TODOlist.java
@@ -1,27 +1,42 @@
 package minesweeper;
 public class TODOlist {
     /**
-     * Separar la vista privilegiada (ver numeros y minas) de la vista de usuario
-     * (ocultar numeros, valles y minas)
-     */
-    /**
+     *
+     * Añadir clase cell con su propio visibility (y seguramente quitar hidemask)
+     *
+     *
      * Interfaz gráfica
-     */
-    /**
-     * Algoritmo de expansión de huecos
-     */
-    /**
-     * Algoritmo de reconocimiento de vecindad de minas
-     */
-    /**
+     *
+     *
      * Unificar Dialogo y Visor para la implementación con GUI
-     */
-    /**
+     *
+     *
      * Meter un monton de excepciones para casos base & shit
-     */
-    /**
+     *
+     *
+     * Añadir clase Game (estado+victory o algo asi)
+     * DONE
+     * 
+     * Algoritmo de expansión de huecos
+     * DONE
+     *
+     *
+     * Algoritmo de reconocimiento de vecindad de minas
+     * DONE
+     *
+     *
      * Meter inclusión automática de minas en el tablero al pasarlas al constructor
      * Algoritmo de distrubución aleatoria veraz
      * DONE
+     *
+     *
+     * Separar la vista privilegiada (ver numeros y minas) de la vista de usuario
+     * (ocultar numeros, valles y minas)
+     * DONE
+     * 
+     * 
+     * 
+     * CHANGELOG
      */
+    
 }
