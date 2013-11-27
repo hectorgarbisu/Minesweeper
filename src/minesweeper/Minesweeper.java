25cf658b666e7750664e71234d8ddb401a80ba84
63990 <63990@63990-PC>
2013-11-27 13:21:50 +0000

    Ultimos cambios: Inyección de dependencia hacia el controlador e Inversión de dependencia con el viewer y el dialog (preparando gui) Banderitas for everyone! Modificada la naturaleza de state (char[][]->cell[][]) El tablero se muestra correctamente para ancho<999 y alto<99
    Pal futuro:
    Poner el conjunto de excepciones correcto
    Permitir que Viewer pueda comportarse como PrivilegedViewer segun datos de entrada (matar PrViewer)
    GUI

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -1,10 +1,33 @@
 package minesweeper;
-import minesweeper.control.*;
 
-public class Minesweeper {    
+import minesweeper.control.*;
+import minesweeper.core.MinesweeperGame;
+import minesweeper.view.MinesweeperGameDialog;
+import minesweeper.view.MinesweeperGameViewer;
+import minesweeper.view.ptext.MinesweeperTextDialog;
+import minesweeper.view.ptext.MinesweeperTextInitializeDialog;
+import minesweeper.view.ptext.MinesweeperTextViewer;
+
+public class Minesweeper {
+    public static final boolean GUI = false;
     
     public static void main(String[] args) {
-        MinesweeperControl minesweeperControl = new MinesweeperControl();
+        MinesweeperGame minesweeperGame = new MinesweeperGame(getInitialValues());
+        MinesweeperGameDialog dialog;
+        MinesweeperGameViewer viewer;
+        if(GUI) ; //dialog = new MinesweeperGraphicDialog;
+                    //viewer = new MinesweeperGraphicViewer;
+        else dialog = new MinesweeperTextDialog();
+             viewer = new MinesweeperTextViewer(minesweeperGame);
+        MinesweeperControl minesweeperControl = new MinesweeperControl(minesweeperGame, dialog, viewer);
         minesweeperControl.execute();
+
+
+    }
+
+    private static int[] getInitialValues() {
+        MinesweeperTextInitializeDialog minesweeperInitializeDialog = new MinesweeperTextInitializeDialog();
+        minesweeperInitializeDialog.execute();
+        return minesweeperInitializeDialog.getValues();
     }
 }
